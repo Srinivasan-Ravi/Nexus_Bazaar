dbc4b32b817eb26c30f4454039bcee7c9b9dfa22
SUREN S <121883751+SUREN239@users.noreply.github.com>
2024-04-01 14:33:53 +0530

    Delete nexus directory

--- nexus/src/main/java/suren/nexus/repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package suren.nexus.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import suren.nexus.model.User;
-
-public interface UserRepo  extends JpaRepository<User,Long>
-{
-
-}
